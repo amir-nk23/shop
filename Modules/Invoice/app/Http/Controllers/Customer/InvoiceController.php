89fd57eb27e4f3f1c7cb30e0d058ba67c336c168
amir <ahosse8383@gmail.com>
2024-06-08 10:38:12 +0330

    edit

---
+++ Modules/Invoice/app/Http/Controllers/Customer/InvoiceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Invoice\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class InvoiceController extends Controller
+{
+
+}
